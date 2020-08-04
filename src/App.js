c4a5449cecbc7cf6d74e44b86084c79ac33e04ce
Kazxsit <kazxsit@gmail.com>
2020-08-04 20:34:38 +0700

    menyelesaikan tugas 12

--- src/App.js
+++ src/App.js
@@ -4,26 +4,6 @@ import Table from './tugas11/daftarBuah'
 import { Timer } from './tugas12/countdown'
 import './App.css';
 
-// Tugas 11
-// class App extends Component {
-//   render() {
-//     let dataHargaBuah = [
-//       {nama: "Semangka", harga: 10000, berat: 1000},
-//       {nama: "Anggur", harga: 40000, berat: 500},
-//       {nama: "Strawberry", harga: 30000, berat: 400},
-//       {nama: "Jeruk", harga: 30000, berat: 1000},
-//       {nama: "Mangga", harga: 30000, berat: 500}
-//     ]
-//     return(
-//       <div className="container">
-//         <Table daftar={dataHargaBuah}/>
-//       </div>
-//     )
-//   }
-// }
-
-
-//Tugas 12
 function App() {
   return (
     <div>
