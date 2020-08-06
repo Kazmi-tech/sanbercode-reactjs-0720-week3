b71682065ddaab51233a33031981bdc9d19983bf
Kazxsit <kazxsit@gmail.com>
2020-08-06 07:59:41 +0700

    tugas 13

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import Table from './tugas11/daftarBuah'
 // import logo from './logo.svg';
 import { Timer } from './tugas12/countdown'
+import TableEdit from './tugas13/EditdaftarBuah'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <header>
-        <Timer start={100}/>
-      </header>
+        <TableEdit/>      
     </div>
-  );
+  ); 
 }
 
 export default App;
